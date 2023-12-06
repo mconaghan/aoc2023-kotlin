6d3350608802e696573ebe6ad14536b6ce0f49b1
Martin Conaghan <mconaghan@gmail.com>
2023-12-06 20:26:15 +0000

    Solve day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,13 +1,13 @@
 fun main() {
 
-    fun findNumberOfSolutionsBetterThanCurrentRecord(raceTime: Int, recordDistance: Int): Int {
+    fun findNumberOfSolutionsBetterThanCurrentRecord(raceTime: Long, recordDistance: Long): Int {
         return (1 until raceTime).map { it * (raceTime - it) }.count { it > recordDistance }
     }
 
     fun part1(input: List<String>): Int {
-        val times = input[0].split("Time:")[1].split("\\s+".toRegex()).filter { it.isNotBlank() }.map { it.toInt() }
+        val times = input[0].split("Time:")[1].split("\\s+".toRegex()).filter { it.isNotBlank() }.map { it.toLong() }
         val distances =
-            input[1].split("Distance:")[1].split("\\s+".toRegex()).filter { it.isNotBlank() }.map { it.toInt() }
+            input[1].split("Distance:")[1].split("\\s+".toRegex()).filter { it.isNotBlank() }.map { it.toLong() }
 
         return times.zip(distances)
             .map { findNumberOfSolutionsBetterThanCurrentRecord(it.first, it.second) }
@@ -15,14 +15,16 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        //TODO
-        return 0
+        val time = input[0].split("Time:")[1].split("\\s+".toRegex()).joinToString("").toLong()
+        val distance = input[1].split("Distance:")[1].split("\\s+".toRegex()).joinToString("").toLong()
+
+        return findNumberOfSolutionsBetterThanCurrentRecord(time, distance)
     }
 
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 288)
 
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 71503)
 
     val input = readInput("Day06")
     print("Part one answer is ") // ?
